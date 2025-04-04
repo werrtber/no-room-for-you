5265b082628371ca20e69f16c7e5f9def19b61ab
0stapKO <159272119+0stapKO@users.noreply.github.com>
2025-04-04 14:45:38 +0300

    Delete backend directory

--- backend/routes/playerListRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const playerListController = require('../controllers/playerListController');
-
-// GET /api/players — отримати список гравців
-router.get('/players', playerListController.getPlayers);
-
-module.exports = router;
--- backend/routes/roomRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const roomController = require('../controllers/roomController');
-
-router.post('/create-room', roomController.createRoom);
-
-module.exports = router;
